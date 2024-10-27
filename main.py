8068d7d5eb0213824c0d9f07cd7edad9b5903eab
v1ctorvinicius <victorviniciuspj@gmail.com>
2024-10-27 11:35:42 -0300

    feat: add ollama options

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ app = FastAPI()
 
 @app.get("/sentence")
 def sentence(word: str = Query(..., description="Word to create the sentence")):
-  message = f"Make a small sentence with the word '{word}' in it."
-  response = ollama.chat(model="tinyllama", messages=[
-    {
-      "role":"user",
-      "content":message
-    }
-  ])
+  message = f"Generate a sentence with the word '{word}'."
+  options = ollama.Options(
+    num_ctx=2048,
+    temperature=0.5,
+    top_k=50,
+    top_p=1,
+    presence_penalty=0.5,
+    frequency_penalty=0.5,
+    stop=["<|user|>", "<|assistant|>"]
+  )
+  response = ollama.chat(
+    model="tinyllama",
+    messages=[
+      {
+        "role":"user",
+        "content":message
+      } 
+    ],
+    options=options
+  )
   return {"[✓] resposta": response.get("message", {}).get("content", '')}
 
 
